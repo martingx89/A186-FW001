8d2255731fbd0d14f5664b53237f98893919d51c
Marcin Adamiak <marcin_89@outlook.com>
2024-10-02 23:15:57 +0200

    ADD: define pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
 
+#define analogPin 0
+#define chargePin 13
+#define dischargePin 11
+#define resistorValue 10000.0F
+
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
