94b9a008b31ab154296eac8c4bb12914197b6204
Marcin Adamiak <marcin_89@outlook.com>
2024-10-02 23:17:12 +0200

    ADD: main script

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,51 @@
 #define dischargePin 11
 #define resistorValue 10000.0F
 
-// put function declarations here:
-int myFunction(int, int);
+unsigned long startTime;
+unsigned long elapsedTime;
+float microFarads;
+float nanoFarads;
 
 void setup()
 {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(chargePin, OUTPUT);
+  digitalWrite(chargePin, LOW);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(chargePin, HIGH);
+  startTime = millis();
+  while (analogRead(analogPin) < 648)
+  {
+  }
 
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
+  elapsedTime = millis() - startTime;
+  microFarads = ((float)elapsedTime / resistorValue) * 1000;
+  Serial.print(elapsedTime);
+  Serial.print(" mS    ");
+
+  if (microFarads > 1)
+  {
+    Serial.print((long)microFarads);
+    Serial.println(" microFarads");
+  }
+
+  else
+  {
+    nanoFarads = microFarads * 1000.0;
+    Serial.print((long)nanoFarads);
+    Serial.println(" nanoFarads");
+    delay(500);
+  }
+
+  digitalWrite(chargePin, LOW);
+  pinMode(dischargePin, OUTPUT);
+  digitalWrite(dischargePin, LOW);
+  while (analogRead(analogPin) > 0)
+  {
+  }
+
+  pinMode(dischargePin, INPUT);
 }
